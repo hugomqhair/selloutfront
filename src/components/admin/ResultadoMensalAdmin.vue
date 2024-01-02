e2cf668a9eb30d1a3de813d0bf7e5876ef377939
Hugo Quinteiro <hugosantista@gmail.com>
2024-01-02 17:37:16 -0300

    implatando de relatorio de curva

--- src/components/admin/ResultadoMensalAdmin.vue
+++ src/components/admin/ResultadoMensalAdmin.vue
@@ -57,15 +57,40 @@
             <!-- </b-col> -->
         </b-row>
         <div>
-            <b-table table-variant="light" head-variant="dark" striped outlined :items="mensal" :fields="fields" small>
+            <b-table table-variant="light" head-variant="dark" striped outlined :items="mensal" :fields="fields" small
+                select-mode="single" selectable @row-selected="selectPromoter">
                 <template #cell(percperiodo)="data">
                     <b :class="data.item.cor">{{ data.value }}</b>
                 </template>
                 <template #cell(objetivoperiodo)="data">
                     <b :class="data.item.cor">{{ data.value }}</b>
                 </template>
+                <template #cell(vlrcurva)="data">
+                    <b :class="data.item.cor">{{ data.value }}</b>
+                </template>
             </b-table>
         </div>
+
+        <!-- Modal para detalhes do Promotor -->
+        <b-modal ref="detalhepromoter" :title="detalhepromoter">
+            <!-- <p class="my-4">Objetivo Atual: {{detalheobj}}%</p> -->
+            <b-col class="m-2">
+                <b-badge variant="success">Objetivo: <b-badge variant="light">{{ detalheobj }} %</b-badge></b-badge>
+                <b-badge variant="success" class="ml-3">Total Curva: <b-badge variant="light">R$ {{ curvaacc }}
+                    </b-badge></b-badge>
+
+            </b-col>
+            <b-table table-variant="light" head-variant="dark" striped outlined :items="mensalDetalhePromoter"
+                :fields="fieldsDetalhe" small id="detalhepromoter">
+                <template #cell(descrprod)="data">
+                    <span>{{ data.value }}</span>
+                </template>
+            </b-table>
+        </b-modal>
+
+
+
+
 
     </div>
 </template>
@@ -76,6 +101,8 @@ export default {
     data() {
         return {
             mensal: [],
+            mensalDetalhe: [],
+            mensalDetalhePromoter: [],
             filtros: {},
             fields: [
                 { key: "promoter", label: "Promoter", sortable: true }
@@ -83,13 +110,26 @@ export default {
                 , { key: "dias", label: "Dias", sortable: true, class: "text-right" }
                 , { key: "objetivo", label: "Objetivo", sortable: true, class: "text-right" }
                 , { key: "objetivoperiodo", label: "% Mês", sortable: true, class: "text-right" }
-                // , { key: "percperiodo", label: "% Dia" }
+                , { key: "vlrcurva", label: "Curva", sortable: true, class: "text-right" }
+
+            ],
+            fieldsDetalhe: [
+                { key: "dia", label: "Dia", sortable: true },
+                { key: "descrprod", label: "Produto", sortable: true },
+                { key: "qtdneg", label: "Qtd", sortable: true, class: "text-right" },
+                { key: "tipo", label: "Curva", sortable: true, class: "text-center" },
+                { key: "vlrcurva", label: "Vlr Curva", sortable: true, class: "text-right" },
             ],
             // objetivo: 80,
             diasmes: 26,
             objetivoTotal: 0,
             quantTotal: 0,
             diasTrabalhados: 0,
+            selected: [],
+            detalhepromoter: '',
+            detalheobj: 0,
+            curvaacc: 0
+
         }
     },
     computed: {
@@ -118,15 +158,19 @@ export default {
         }
     },
     created() {
+        //VOLTAR esse código
         //console.log(this.$store.state.login)
-        if (!this.$store.state.login.token) {
-            this.$router.push(`/Login`)
-        } else {
-            let now = new Date()
-            this.filtros.ano = now.getFullYear()
-            this.filtros.mes = String(now.getMonth() + 1).padStart(2, '0')
-            //this.resultadoMensal()
-        }
+        // if (!this.$store.state.login.token) {
+        //     this.$router.push(`/Login`)
+        // } else {
+        //     let now = new Date()
+        //     this.filtros.ano = now.getFullYear()
+        //     this.filtros.mes = String(now.getMonth() + 1).padStart(2, '0')
+        // }
+        let now = new Date()
+        this.filtros.ano = now.getFullYear()
+        this.filtros.mes = String(now.getMonth() + 1).padStart(2, '0')
+
     },
     methods: {
         // Objetivo() {
@@ -138,6 +182,7 @@ export default {
                 this.objetivoTotal = 0
                 this.quantTotal = 0
                 this.diasTrabalhados = 0
+                // console.log(res.data)
                 this.mensal = res.data.map(val => {
                     val.objetivo = (val.objetivo === 0 ? 99 : val.objetivo)
                     let objetivoperiodo = ((val.qtdneg / val.objetivo) * 100).toFixed(2)
@@ -157,12 +202,64 @@ export default {
                     }
                     return { ...val, objetivoperiodo, percperiodo, cor }
                 })
+                // Carregando detalhe por promoter
+                this.$http.get(`consulta?operacao=resultadoAdminDetalhe&ano=${this.filtros.ano}&mes=${this.filtros.mes}`).then(res => {
+                    //console.log('Detalhe', res.data)
+                    this.mensalDetalhe = res.data
+                    let curvaacc
+                    let vlrcurva
+                    this.mensal = this.mensal.map(v => {
+                        curvaacc = 0
+                        vlrcurva = 0
+                        this.mensalDetalhe.filter(filt => filt.promoter === v.promoter).forEach(vdet => {
+                            //console.log(v.promoter === vdet.promoter ? v.promoter +' - '+  vdet.promoter : null)
+                            if (parseFloat(v.objetivoperiodo) >= 85 && parseFloat(v.objetivoperiodo) < 95) {
+                                vlrcurva = (vdet.tipo === 'A' ? 1 : 0.75) * vdet.qtdneg
+                            } else if (parseFloat(v.objetivoperiodo) >= 95 && parseFloat(v.objetivoperiodo) < 100) {
+                                vlrcurva = (vdet.tipo === 'A' ? 1.5 : 1) * vdet.qtdneg
+                            } else if (parseFloat(v.objetivoperiodo) >= 100) {
+                                vlrcurva = (vdet.tipo === 'A' ? 2 : 1.5) * vdet.qtdneg
+                            }
+                            curvaacc += vlrcurva
+                        })
+                        //console.log('curva', curvaacc)
+                        return { ...v, vlrcurva: curvaacc.toFixed(2) }
+                    })
+                })
+
                 this.$store.state.loading = !this.$store.state.loading
             })
                 .catch(err => {
                     console.log('ERRO ***', err)
                     this.$store.state.mensagens = [{ texto: 'Falha de Servidor, informar ao TI', tipo: 'danger', tempo: 5, dismissCountDown: 0 }]
                 })
+
+            // // Carregando detalhe por promoter
+            // this.$http.get(`consulta?operacao=resultadoAdminDetalhe&ano=${this.filtros.ano}&mes=${this.filtros.mes}`).then(res => {
+            //     console.log('Detalhe', res.data)
+            //     this.mensalDetalhe = res.data
+            //     let curvaacc
+            //     let vlrcurva
+            //     this.mensal = this.mensal.map(v => {
+            //         curvaacc = 0
+            //         vlrcurva = 0
+            //         this.mensalDetalhe.filter(filt => filt.promoter === v.promoter).forEach(vdet => {
+            //             //console.log(v.promoter === vdet.promoter ? v.promoter +' - '+  vdet.promoter : null)
+            //                 if(parseFloat(v.objetivoperiodo)>=85 && parseFloat(v.objetivoperiodo) < 95 ){
+            //                     vlrcurva = (vdet.tipo === 'A' ? 1 : 0.75) * vdet.qtdneg
+            //                 } else if (parseFloat(v.objetivoperiodo)>=95 && parseFloat(v.objetivoperiodo)<100 ){
+            //                     vlrcurva = (vdet.tipo === 'A' ? 1.5 : 1) * vdet.qtdneg
+            //                 } else if (parseFloat(v.objetivoperiodo)>=100){
+            //                     vlrcurva = (vdet.tipo === 'A' ? 2 : 1.5) * vdet.qtdneg
+            //                 }
+            //                 curvaacc += vlrcurva
+            //         })
+            //         //console.log('curva', curvaacc)
+            //         return {...v, vlrcurva: curvaacc.toFixed(2)}
+            //     })
+            // })
+            //resultadoAdminDetalhe
+
         },
 
         getFiltros(filtros) {
@@ -171,6 +268,31 @@ export default {
             //this.mensal = []
             this.resultadoMensal()
 
+        },
+        selectPromoter(items) {
+            // console.log('Selecionou', items)
+            this.detalhepromoter = items
+            this.$refs['detalhepromoter'].show()
+            this.detalhepromoter = items[0].promoter
+            this.detalheobj = items[0].objetivoperiodo
+            //console.log(this.detalhepromoter, this.detalheobj)
+            let curvaacc = 0
+            this.mensalDetalhePromoter = this.mensalDetalhe.filter(v => v.promoter === items[0].promoter)
+                .map(v => {
+                    let vlrcurva = 0
+                    //console.log('% float', parseFloat(items[0].objetivoperiodo))
+                    if (parseFloat(items[0].objetivoperiodo) >= 85 && parseFloat(items[0].objetivoperiodo) < 95) {
+                        vlrcurva = (v.tipo === 'A' ? 1 : 0.75) * v.qtdneg
+                    } else if (parseFloat(items[0].objetivoperiodo) >= 95 && parseFloat(items[0].objetivoperiodo) < 100) {
+                        vlrcurva = (v.tipo === 'A' ? 1.5 : 1) * v.qtdneg
+                    } else if (parseFloat(items[0].objetivoperiodo) >= 100) {
+                        vlrcurva = (v.tipo === 'A' ? 2 : 1.5) * v.qtdneg
+                    }
+                    curvaacc += vlrcurva
+                    return { ...v, dia: v.dtmov.substr(8, 2), vlrcurva: vlrcurva.toFixed(2) }
+                })
+            this.curvaacc = curvaacc
+            //console.log('detalhe', this.mensalDetalhePromoter)
         }
 
     },
@@ -183,5 +305,9 @@ export default {
 <style scoped>
 .resultadoMensalAdmin {
     /* color: antiquewhite; */
+}
+
+#detalhepromoter {
+    font-size: 0.75em;
 }
 </style>
